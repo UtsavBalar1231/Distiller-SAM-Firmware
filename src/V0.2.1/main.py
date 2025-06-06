ec7cac44dc3569adde90aa5b369d0283b0943fd7
Kevin Zhang <01001010kevin@gmail.com>
2025-06-05 19:07:13 -0700

    add 0.2.1

--- src/V0.2.1/main.py
+++ src/V0.2.1/main.py
@@ -3,7 +3,6 @@
 #Version: 0.1.0
 #Description: This is the main program for the RP2040 SAM
 
-#TODO EINK BLOCK CORE 1 from complete if eink broken
 import machine
 import utime
 from eink_driver_sam import einkDSP_SAM
@@ -12,23 +11,29 @@ from machine import WDT
 import math
 import neopixel
 import json
-from uart_protocol import PamirProtocol
 
-BTN_UP_MASK     = 0x01
-BTN_DOWN_MASK   = 0x02
-BTN_SELECT_MASK = 0x04
-BTN_POWER_MASK  = 0x08
 
-# # Reset PMIC, DO NO REMOVE THIS BLOCK, Covers Non Battery Non Boost Version Board
-# pmic_enable.value(0) # Pull down pin
-# utime.sleep(0.01) # Keep low for 0.01 second
-# pmic_enable.init(mode=machine.Pin.IN)
-# # END OF PMIC RESET BLOCK
+# Reset PMIC, DO NO REMOVE THIS BLOCK
+pmic_enable = machine.Pin(3, machine.Pin.IN, pull=None)
+# END OF PMIC RESET BLOCK
 
 PRODUCTION = True  #for production flash, set to true for usb debug
-PRINT_DEBUG_UART = True #for UART debug, set to true for UART debug
+UART_DEBUG = False #for UART debug, set to true for UART debug
 LUT_MODE = True #for LUT mode, set to true for LUT mode
-debounce_time = 50 # Debounce time in milliseconds
+
+#Instruction Set
+EncodeTable = {"BTN_UP": 0b00000001, "BTN_DOWN": 0b00000010, "BTN_SELECT": 0b00000100, "SHUT_DOWN": 0b00001000}
+USB_SWITCH_TABLE = {"SAM_USB": [0,0], "SOM_USB": [1,0]} # S, OE_N
+# usb_switch_oe_n = machine.Pin(19, machine.Pin.OUT, value = 0)
+usb_switch_s = machine.Pin(23, machine.Pin.OUT, value = 0)
+
+def switch_usb(usb_type):
+    if usb_type in USB_SWITCH_TABLE:
+        S, _ = USB_SWITCH_TABLE[usb_type]
+        usb_switch_s.value(S)
+        # usb_switch_oe_n.value(OE_N)
+    else:
+        print(f"Invalid USB type: {usb_type}")
 
 
 wdt = WDT(timeout=2000)
@@ -36,87 +41,26 @@ wdt = WDT(timeout=2000)
 selectBTN = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
 upBTN = machine.Pin(17, machine.Pin.IN, machine.Pin.PULL_DOWN)
 downBTN = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_DOWN)
-einkStatus = machine.Pin(9, machine.Pin.OUT, value = 1)
-einkMux = machine.Pin(22, machine.Pin.OUT, value = 1)
-nukeUSB = machine.Pin(19, machine.Pin.OUT, value = 0)
-pmic_enable = machine.Pin(3, machine.Pin.OUT)
-i2c = machine.I2C(0, sda=machine.Pin(24), scl=machine.Pin(25))
-
-# Check if battery gauge is present at address 85 (0x55)
-devices = i2c.scan()
-BATTERY_MODE = 85 in devices
-
-if BATTERY_MODE:
-    from battery import BQ27441
-    battery = BQ27441(i2c)
-    battery.initialise(design_capacity_mAh=3000,
-                 terminate_voltage_mV=3200,
-                 CALIBRATION=True)      # learn on this board
-
+einkStatus = machine.Pin(9, machine.Pin.OUT)
+einkMux = machine.Pin(22, machine.Pin.OUT)
+sam_interrupt = machine.Pin(2, machine.Pin.OUT)
+# Debounce time in milliseconds
+debounce_time = 50
 
 if PRODUCTION:
-    nukeUSB.high() # Disable SAM USB
-    
+    switch_usb("SOM_USB") # Disable SAM USB
 # Setup UART0 on GPIO0 (TX) and GPIO1 (RX)
 uart0 = machine.UART(0, baudrate=115200, tx=machine.Pin(0), rx=machine.Pin(1))
 einkMux.low()  # EINK OFF
 einkStatus.low()  # SOM CONTROL E-INK
-eink = einkDSP_SAM() # Initialize eink
-
-
-# Add lock for shared variable
-power_status = False
-core1_task_interrupt_lock = _thread.allocate_lock()
-core1_task_interrupt = False
-eink_lock = _thread.allocate_lock()
-einkRunning = False
-neopixel_lock = _thread.allocate_lock()  # Add lock for neopixel operations
-uart_lock = _thread.allocate_lock()  # Add lock for UART handling
-current_neopixel_thread = None  # Track current neopixel thread
-neopixel_running = False  # Flag to control current neopixel sequence
-
-pamir_protocol = PamirProtocol(uart0, debug=False, wdt=wdt)
-
-def send_button_state():
-    state_byte = 0
-    state_byte |= get_debounced_state(selectBTN) * BTN_SELECT_MASK
-    state_byte |= get_debounced_state(upBTN)  * BTN_UP_MASK
-    state_byte |= get_debounced_state(downBTN) * BTN_DOWN_MASK
-    
-    debug_print(f"BUTTON STATE: {state_byte}")
-    pamir_protocol.send_button_state(state_byte)
-    
-    
-# Interrupt handler for down button
-def button_handler(pin):
-    if debounce(pin):
-        send_button_state()
-
-
-# Set up interrupt handlers
-selectBTN.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
-upBTN.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
-downBTN.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
-
+eink = einkDSP_SAM()
 
 print("Starting...")
 
-def power_on_som():
-    global power_status
-    pmic_enable.init(mode=machine.Pin.IN)
-    power_status = True
-    
-def power_off_som():
-    global power_status
-    pmic_enable.init(mode=machine.Pin.OPEN_DRAIN)
-    pmic_enable.low() # Pull down pin
-    power_status = False
-
-
 # Function to handle UART debug messages
 def debug_print(message):
-    if PRINT_DEBUG_UART:
-        pamir_protocol.send_debug_text(message)
+    if UART_DEBUG:
+        uart0.write(message)
     print(message)
 
 # Begin of neopixel
@@ -139,6 +83,13 @@ def set_color(np, color, brightness=None, index=None):
         np[index] = (r, g, b)
     np.write()
 
+# Add lock for shared variable
+eink_lock = _thread.allocate_lock()
+einkRunning = False
+neopixel_lock = _thread.allocate_lock()  # Add lock for neopixel operations
+uart_lock = _thread.allocate_lock()  # Add lock for UART handling
+current_neopixel_thread = None  # Track current neopixel thread
+neopixel_running = False  # Flag to control current neopixel sequence
 
 def handle_neopixel_sequence(np, data):
     global neopixel_running
@@ -174,6 +125,7 @@ def handle_neopixel_sequence(np, data):
     
     neopixel_running = False
 
+
 # Function to debounce button press
 def debounce(pin):
     state = pin.value()
@@ -185,16 +137,48 @@ def debounce(pin):
 def get_debounced_state(pin):
     return pin.value() and debounce(pin) 
 
+def send_button_state():
+    state_byte = 0
+    state_byte |= get_debounced_state(selectBTN) * EncodeTable["BTN_SELECT"]
+    state_byte |= get_debounced_state(upBTN)  * EncodeTable["BTN_UP"]
+    state_byte |= get_debounced_state(downBTN) * EncodeTable["BTN_DOWN"]
+    if not PRODUCTION:
+        print(f"state_byte: {state_byte}")
+    uart0.write(bytes([state_byte]))
+
+# Interrupt handler for down button
+def button_handler(pin):
+    if debounce(pin):
+        send_button_state()
+
+def loading_terminator(pin):
+    global einkRunning
+    if einkRunning:
+        einkRunning = False
+        eink.de_init()
+
+
+# Set up interrupt handlers
+selectBTN.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
+upBTN.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
+downBTN.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
+sam_interrupt.irq(trigger=machine.Pin.IRQ_RISING, handler=loading_terminator)
 
 # Initialize neopixel with just 1 LED
 np = init_neopixel(pin=20, num_leds=1, brightness=0.5)
 debug_print(f"[RP2040 DEBUG] Initialized {len(np)} NeoPixel\n")
 neopixel_running = True
 
+einkStatus.high() # provide power to eink
+einkMux.high() # SAM CONTROL E-INK
+debug_print("StartScreen\n")
+
+# Shared flag to coordinate thread handoff
+thread_handoff_complete = False
 
 # Thread to handle both eink and UART tasks
 def core1_task():
-    global einkRunning, neopixel_running, core1_task_interrupt
+    global einkRunning, thread_handoff_complete, neopixel_running
     
     # First, run the eink task
     try:
@@ -241,16 +225,15 @@ def core1_task():
             einkRunning = False
         einkMux.low()
     
-
+    # Signal that eink is done and we can transition to UART
+    thread_handoff_complete = True
+    
     # Now run the UART task
     debug_print("[RP2040 DEBUG] Starting UART handling on core1\n")
     uart_buffer = ""
     
     # UART handling loop
-    while False:
-        with core1_task_interrupt_lock:
-            if core1_task_interrupt:
-                break
+    while True:
         try:
             if uart0.any():
                 raw_data = uart0.read(1)
@@ -289,95 +272,35 @@ def core1_task():
         
         wdt.feed()
         utime.sleep_ms(1)
-    
-    debug_print("Core1 task completed\n")
 
-def check_for_power_on():
-    global einkRunning, core1_task_interrupt
-    if debounce(selectBTN) and selectBTN.value() == 1 and upBTN.value() == 0 and downBTN.value() == 0:
-        start_time = utime.ticks_ms()
-        while utime.ticks_diff(utime.ticks_ms(), start_time) < 2000:
-            if selectBTN.value() == 0:
-                break
-            wdt.feed()
-            utime.sleep_ms(10)
-        if utime.ticks_diff(utime.ticks_ms(), start_time) >= 2000 and power_status == False:
-            power_on_som()
-            einkMux.high() # SAM CONTROL E-INK 
-            einkStatus.high() # provide power to eink
-            if PRODUCTION:
-                nukeUSB.high() # Disable SAM USB
-            
-            # Turn on the power on loading screen
-            print("einkRunning: ", einkRunning)
-            if einkRunning == False:
-                try:
-                    # Reset interrupt flag before starting new thread
-                    with core1_task_interrupt_lock:
-                        core1_task_interrupt = False
-                    
-                    # Ensure eink is properly initialized
-                    einkRunning = True
-                    _thread.start_new_thread(core1_task, ())
-                    print("Started core1 task")
-                except Exception as e:
-                    print(f"Exception {e}")
-                    eink.de_init()
-                    einkRunning = False
-                    # Reset interrupt flag in case of error
-                    with core1_task_interrupt_lock:
-                        core1_task_interrupt = False
+# Start the combined thread on core1
+_thread.start_new_thread(core1_task, ())
+print("Started core1 task")
 
-def check_for_power_off():
-    global core1_task_interrupt
-    if debounce(upBTN) and selectBTN.value() == 1:
-        start_time = utime.ticks_ms()
-        while utime.ticks_diff(utime.ticks_ms(), start_time) < 8000: 
-            if upBTN.value() == 0 or selectBTN.value() == 0:
-                break
-            if utime.ticks_diff(utime.ticks_ms(), start_time) >= 2000:
-                # This only send soft shutdown command to the Linux. 
-                # The actual power off is handled by the Linux.
-                pamir_protocol.send_shutdown_command()
-                
-                # When the uart recieve confirmation from Linux that Shutdown is executed from its end
-                # The RP2040 will turn off the eink and mux. This will be a forced shutdown.
-                #TODO: from the core1 task
-            wdt.feed()
-            utime.sleep_ms(10)
-        if utime.ticks_diff(utime.ticks_ms(), start_time) >= 8000:
-            # After 8 seconds, turn off the eink and mux. This will be a forced shutdown.
-            einkMux.high() # SAM CONTROL E-INK 
-            einkStatus.low() # disable power to eink
-            power_off_som()
-            
-            if PRODUCTION:
-                nukeUSB.low() # Rejoin SAM to USB
-            with core1_task_interrupt_lock:
-                core1_task_interrupt = True
-           
 # Clean main loop
 while True:
     wdt.feed()
     
-    check_for_power_on()
-    check_for_power_off()
-    
-    if BATTERY_MODE:
-        try:
-            soc = battery.remain_capacity()
-            voltage = battery.voltage_V()
-            temp = battery.temp_C()
-            current = battery.avg_current_mA()
-            print(f"SOC: {soc}%  V: {voltage}  T: {temp} A: {current}")
-            pamir_protocol.send_debug_text(f"SOC: {soc}%  V: {voltage}  T: {temp} A: {current}")
-        except Exception as e:
-            print(f"Battery read error: {e}")
-            pamir_protocol.send_debug_text(f"Battery read error: {e}")
-        utime.sleep_ms(1000)
+    # Only proceed with normal operation after handoff is complete
+    if thread_handoff_complete:
+        if debounce(upBTN) and selectBTN.value() == 1:
+            start_time = utime.ticks_ms()
+            while utime.ticks_diff(utime.ticks_ms(), start_time) < 10000:
+                if upBTN.value() == 0 or selectBTN.value() == 0:
+                    break
+                if utime.ticks_diff(utime.ticks_ms(), start_time) >= 2000:
+                    uart0.write(f"{EncodeTable['SHUT_DOWN']}\n")
+                wdt.feed()
+                utime.sleep_ms(10)
+            if utime.ticks_diff(utime.ticks_ms(), start_time) >= 10000:
+                einkStatus.low()  
+                einkMux.low() # SOM CONTROL E-INK
+                uart0.write("xSAM_USB\n")
+                if PRODUCTION:
+                    switch_usb("SAM_USB")
+                einkRunning = False
     
     utime.sleep_ms(1)
-
 
 
 
